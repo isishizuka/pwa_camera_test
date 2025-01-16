1df61f6c6285b769366ac26872d5e602284e38d9
isishizuka <isishizuka@gmail.com>
2025-01-17 06:09:45 +0900

    change logic

--- sw.js
+++ sw.js
@@ -1,21 +1,28 @@
 var CACHE_NAME = 'pwa-camera-test-caches';
 var urlsToCache = ['/index.html', '/js/camera.js'];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
+    // Cache Storage にリソースをキャッシュし直す
+    event.waitUntil(PopulateResourcesInCacheStorage());
     event.waitUntil(
         caches
             .open(CACHE_NAME)
-            .then(function(cache) {
+            .then(function (cache) {
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListern('activate', e => {
+    // 不要なリソースを Cache Storage から削除する
+    e.waitUntil(DeleteOutdatedResourcesInCacheStorage());
+});
+
+self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches
             .match(event.request)
-            .then(function(response) {
+            .then(function (response) {
                 return response ? response : fetch(event.request);
             })
     );
